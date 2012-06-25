e9b064c37a50a814616fd44d531a9b6fd78244dd
Sampath <vinay@DreamChaser>
2012-06-25 19:37:52 +0530

    Updated...

--- HelloWorld/src/HellowWorld.java
+++ HelloWorld/src/HellowWorld.java
@@ -5,7 +5,7 @@ public class HellowWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World committed to GIT");
 
 	}
 
